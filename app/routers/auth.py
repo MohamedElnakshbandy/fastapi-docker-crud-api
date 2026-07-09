235ace43a655881e73e96490d3a01008a02fdb5c
Mohamed <mohamedayman22@ymail.com>
2026-07-09 12:22:00 +0300

    feat: implement JWT authentication and protected routes

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,12 +1,35 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
+from app.auth import create_access_token, verify_password, decode_access_token, oauth2_scheme
 from app import crud, database, schemas
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(
     prefix="/auth",
     tags=["Auth"]
 )
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(database.get_db)
+):
+    email = decode_access_token(token)
+
+    if email is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid or expired token"
+        )
+
+    user = crud.get_user_by_email(db, email)
+
+    if user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="User not found"
+        )
+
+    return user
 
 
 @router.post("/register", response_model=schemas.UserResponse)
@@ -23,3 +46,38 @@ def register(
         )
 
     return crud.create_user(db, user)
+
+@router.post("/login", response_model=schemas.Token)
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(database.get_db)
+):
+    db_user = crud.get_user_by_email(db, form_data.username)
+
+    if db_user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    if not verify_password(form_data.password, db_user.hashed_password):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    access_token = create_access_token(
+        data={"sub": db_user.email}
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
+
+
+@router.get("/me", response_model=schemas.UserResponse)
+def get_me(
+    current_user = Depends(get_current_user)
+):
+    return current_user
